c46a5aac8342ec61e8257aba41a18fe7b1c49eef
arshad <arshad@thinkebiz.net>
2023-07-13 19:02:31 +0530

    Add Modules and done Principal Apis

--- models/principal.js
+++ models/principal.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var teacherSchema = new Schema({
+var principalSchema = new Schema({
     "title" : String,
     "firstName": String,
     "lastName": String,
@@ -14,20 +14,16 @@ var teacherSchema = new Schema({
     "altEmail": String,
     "gender": String,
     "password": String,
-    "principalId":{ type: Schema.Types.ObjectId, ref: "principal" },
-    "schoolId":{ type: Schema.Types.ObjectId, ref: "school" },
-    "teacherRole": String,
-    "class": String,
-    "year": String,
+    "principalRole": {type: String, default: "Principal" },
     "address": String,
     "city": String,
     "zipcode": String,
     "state": String,
-    "role":  {type: String, default: "Teacher" },
+    "role":  {type: String, default: "Principal" },
     "country": {type: String, default: "India" },
     "status":  {type: String, default: "Active" },
     "isDeleted": {type: Boolean, default: false },
     "createdDate": { type: Date, default: Date.now },
     "updatedDate": { type: Date, default: Date.now },
 });
-module.exports = mongoose.model('teacher', teacherSchema);
+module.exports = mongoose.model('principal', principalSchema);
